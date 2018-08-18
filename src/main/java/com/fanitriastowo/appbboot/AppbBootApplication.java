ea3bbc157ce278ef6002a7b652ee7b6d666b1bb4
fanitriastowo <fani.triastowo@gmail.com>
2018-08-19 01:09:18 +0700

    initial commit

---
+++ src/main/java/com/fanitriastowo/appbboot/AppbBootApplication.java
@@ -0,0 +1,12 @@
+package com.fanitriastowo.appbboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppbBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppbBootApplication.class, args);
+	}
+}
